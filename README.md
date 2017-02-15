c0641e2fbaea471b5ae1647ffc062619bda699fe
Dallas Read <dallas@excitecreative.ca>
2017-02-15 19:12:08 -0400

    Optionally set the loading mode, with smart fallback.
    ```
    loadExternal('https://js.stripe.com/v2/', 'js', function() {
        // alert('Stripe is loaded!');
    });
    ```

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ loadExternal('https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css',
     // alert('CSS Reset is loaded & applied!');
 });
 ```
+
+LoadExternal does its best to determine the type of file you wish to load, but you can also explicitly define the mode (currently accepts js or css):
+
+```
+loadExternal('https://js.stripe.com/v2/', 'js', function() {
+    // alert('Stripe is loaded!');
+});
+```
--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-function externalLoader(url, callback) {
-    var script = null;
+function loadExternal(url, mode, callback) {
+    var script = undefined;
 
-    if (url.slice(url.lastIndexOf('.') + 1, url.length) === 'js') {
-        script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = url;
-    } else {
+    if (typeof mode === 'function') {
+        callback = mode;
+        mode = /css/.test(url) ? 'css' : 'js';
+    }
+
+    if (mode === 'css') {
         script = document.createElement( 'link' );
         script.setAttribute( 'href', url );
         script.setAttribute( 'rel', 'stylesheet' );
         script.setAttribute( 'type', 'text/css' );
+    } else {
+        script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
     }
 
     if (script.readyState) {
         script.onreadystatechange = function() {
             if (script.readyState === 'loaded' || script.readyState === 'complete') {
-                script.onreadystatechange = null;
+                script.onreadystatechange = undefined;
 
                 if (typeof callback === 'function') {
                     callback();
@@ -33,8 +38,10 @@ function externalLoader(url, callback) {
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
+if (typeof window !== 'undefined') {
+    window.loadExternal = window.externalLoader = loadExternal;
+}
+
 if (typeof module !== 'undefined') {
-    module.exports = externalLoader;
-} else {
-    window.externalLoader = externalLoader;
+    module.exports = loadExternal;
 }
